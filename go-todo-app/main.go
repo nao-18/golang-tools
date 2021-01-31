5c2b4e337946895fd5517e1e3f59e318937f517b
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-31 14:31:07 +0900

    create http

--- go-todo-app/main.go
+++ go-todo-app/main.go
@@ -1,77 +1,13 @@
 package main
 
-import "go-tools/go-todo-app/app/models"
+import (
+	"fmt"
+	"go-tools/go-todo-app/app/controllers"
+	"go-tools/go-todo-app/app/models"
+)
 
 func main() {
-	/*
-		fmt.Println(config.Config.Port)
-		fmt.Println(config.Config.SQLDriver)
-		fmt.Println(config.Config.DbName)
-		fmt.Println(config.Config.LogFile)
+	fmt.Println(models.Db)
 
-		log.Println("test")
-	*/
-
-	/*
-		fmt.Println(models.Db)
-
-		u := &models.User{}
-		u.Name = "test3 name"
-		u.Email = "test3@example.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
-
-		u.CreateUser()
-	*/
-
-	/*
-		//ユーザ削除
-		u, _ := models.GetUser(1)
-		fmt.Println(u)
-
-		u.Name = "Test2"
-		u.Email = "test2@example.com"
-		u.UpdateUser()
-		u, _ = models.GetUser(1)
-		fmt.Println(u)
-
-		u.DeleteUser()
-		u, _ = models.GetUser(1)
-		fmt.Println(u)
-	*/
-
-	/*
-		user, _ := models.GetUser(2)
-		user.CreateTodo("TEST CONTENT")
-	*/
-
-	/*
-		t, _ := models.GetTodo(1)
-		fmt.Println(t)
-
-	*/
-
-	/*
-		user, _ := models.GetUser(2)
-		user.CreateTodo("TEST CONTENT3")
-	*/
-
-	/*
-		todos, _ := models.GetTodos()
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	/*
-		todos, _ := user.GetTodosByUser()
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	//todo更新
-	t, _ := models.GetTodo(1)
-	t.Content = "update todo"
-	t.UpdateTodo()
+	controllers.StartMainServer()
 }
