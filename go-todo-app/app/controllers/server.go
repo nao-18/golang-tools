47d5252557350d84894f97a67e0ba8274c212846
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-31 21:00:16 +0900

    create todo edit and update.

--- go-todo-app/app/controllers/server.go
+++ go-todo-app/app/controllers/server.go
@@ -6,6 +6,8 @@ import (
 	"go-tools/go-todo-app/config"
 	"html/template"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 func GenerateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
@@ -28,6 +30,25 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 	return sess, err
 }
 
+var validPath = regexp.MustCompile("^/todos/(edit|update)/([0-9]+)$")
+
+func parseURL(fu func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// /todos/edit/1
+		q := validPath.FindStringSubmatch(r.URL.Path)
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+		qi, err := strconv.Atoi(q[2])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fu(w, r, qi)
+	}
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -39,5 +60,7 @@ func StartMainServer() error {
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
