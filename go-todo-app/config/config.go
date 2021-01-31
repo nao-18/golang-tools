438f5989ef343385ee26e153c7c1316d7adafe4e
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-31 14:58:38 +0900

    add resource files.

--- go-todo-app/config/config.go
+++ go-todo-app/config/config.go
@@ -11,24 +11,26 @@ type ConfigList struct {
 	SQLDriver string
 	DbName    string
 	LogFile   string
+	Static    string
 }
 
 var Config ConfigList
 
-func init()  {
+func init() {
 	LoadConfig()
 	utils.LoggingSettings(Config.LogFile)
 }
 
-func LoadConfig()  {
+func LoadConfig() {
 	cfg, err := ini.Load("./config/config.ini")
 	if err != nil {
 		log.Fatal(err)
 	}
 	Config = ConfigList{
-		Port: cfg.Section("web").Key("port").MustString("8080"),
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
 		SQLDriver: cfg.Section("db").Key("driver").String(),
-		DbName: cfg.Section("db").Key("name").String(),
-		LogFile: cfg.Section("web").Key("logfile").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
 	}
 }
